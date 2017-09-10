29b6bbf541d0086c819736a5b71f85b1eadb133a
siyuan <siyuan@mac-mini.com>
2017-09-10 00:36:33 -1000

    Add in helper function -- look up table logic

--- EAL_Surfer/surferTable/views.py
+++ EAL_Surfer/surferTable/views.py
@@ -1,39 +1,120 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-
 # Create your views here.
 # -*- coding: utf-8 -*-
-
-# Create your views here.
-#from django.http import HttpResponse
-#from django.template import Context, loader
 import sqlite3 as db
 from django.shortcuts import render
+import math
 
-
-conn = db.connect('EAL_SURFER_TABLES_DB.sqlite3')
+dbName = 'EAL_SURFER_TABLES_DB.sqlite3'
+configTableName = 'allchemicals'
+# connect to db and setting connection settings
+conn = db.connect(dbName)
 conn.row_factory = lambda cursor, row: row[0]
 conn.text_factory = str
 c = conn.cursor()
+# get drop down list values from specific columns of config table
 listOfchemicalNames = c.execute('SELECT c3 FROM allchemicals').fetchall()
-listOfchemicalNames.pop(0)
+listOfchemicalNames.pop(0) # pop off the column header
 listOfCASNames = c.execute('SELECT c2 FROM allchemicals').fetchall()
-listOfCASNames.pop(0)
+listOfCASNames.pop(0) # pop off the column header
+
 
 def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    #template = loader.get_template("index.html")
-    #return HttpResponse(template.render())
-
-    #if request.POST.get("base", "") == 'chemical':
-    #    dropDownlistValue = listOfchemicalNames
-    #
     if request.method == 'GET':
-        print request.GET.get('LandUse','')
-        print request.GET.get('GroundWaterUtility','')
-        print request.GET.get('DistanceToNearest','')
-        print request.GET.get('ContaminantType','')
-        print request.GET.get('ContaminantName','')
+        landUse = request.GET.get('LandUse', '')
+        groundWaterUtility = request.GET.get('GroundWaterUtility', '')
+        distanceToNearest = request.GET.get('DistanceToNearest', '')
+        contaminantType = request.GET.get('ContaminantType', '')
+        contaminantName = request.GET.get('ContaminantName', '')
+        # convert CAS to chemical name
+        if contaminantType == 'CAS':
+            contaminantName = convertCASNameToChemicalName(contaminantName)
+        lookUpValue = dbLookUp("c3", "SummaryTableA", contaminantName)
+        # debug print
+        # print lookUpValue
 
-    return render(request, 'index.html', {'listOfchemicalNames': listOfchemicalNames, 'listOfCASNames' : listOfCASNames })
+    return render(request, 'index.html', {'listOfchemicalNames': listOfchemicalNames, 'listOfCASNames': listOfCASNames})
+
+
+# Helper function to look up mapping of CAS to chemical name
+def convertCASNameToChemicalName(contaminantName):
+    conn = db.connect(dbName)
+    c = conn.cursor()
+    conn.text_factory = str
+    c.execute('SELECT c3 FROM ' + configTableName + ' WHERE c2="{cn}"'. \
+              format(cn=contaminantName))
+    result = c.fetchall()
+    return result[0][0]
+
+
+# Helper function to look up db (translate of vlookup functionality)
+def dbLookUp(column, table_name, contaminantName):
+    conn = db.connect(dbName)
+    c = conn.cursor()
+    c.execute('SELECT {col} FROM {tn} WHERE c1="{cn}"'. \
+              format(col=column, tn=table_name, cn=contaminantName))
+    result = c.fetchall()
+    return to_precision(float(result[0][0]), 2)
+
+
+# Helper function
+def to_precision(x, p):
+    """
+    returns a string representation of x formatted with a precision of p
+
+    Based on the webkit javascript implementation taken from here:
+    https://code.google.com/p/webkit-mirror/source/browse/JavaScriptCore/kjs/number_object.cpp
+    """
+    x = float(x)
+
+    if x == 0.:
+        return "0." + "0" * (p - 1)
+
+    out = []
+
+    if x < 0:
+        out.append("-")
+        x = -x
+
+    e = int(math.log10(x))
+    tens = math.pow(10, e - p + 1)
+    n = math.floor(x / tens)
+
+    if n < math.pow(10, p - 1):
+        e = e - 1
+        tens = math.pow(10, e - p + 1)
+        n = math.floor(x / tens)
+
+    if abs((n + 1.) * tens - x) <= abs(n * tens - x):
+        n = n + 1
+
+    if n >= math.pow(10, p):
+        n = n / 10.
+        e = e + 1
+
+    m = "%.*g" % (p, n)
+
+    if e < -2 or e >= p:
+        out.append(m[0])
+        if p > 1:
+            out.append(".")
+            out.extend(m[1:p])
+        out.append('e')
+        if e > 0:
+            out.append("+")
+        out.append(str(e))
+    elif e == (p - 1):
+        out.append(m)
+    elif e >= 0:
+        out.append(m[:e + 1])
+        if e + 1 < len(m):
+            out.append(".")
+            out.extend(m[e + 1:])
+    else:
+        out.append("0.")
+        out.extend(["0"] * -(e + 1))
+        out.append(m)
+
+    return "".join(out)
