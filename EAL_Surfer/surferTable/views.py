7aea277f7b79c2d23479a67f05a449c56d11f7b3
cn808 <cn808@macbook.com>
2017-09-13 22:37:50 -1000

    Add in pdf download functionality

--- EAL_Surfer/surferTable/views.py
+++ EAL_Surfer/surferTable/views.py
@@ -7,6 +7,7 @@ from compiler import surferTableInput
 from utility import *
 from surferTable.models import Allchemicals
 import pdfkit
+from django.templatetags.static import static
 
 def index(request):
 
@@ -30,7 +31,23 @@ def index(request):
                     'distanceToNearest': distanceToNearest,
                     'contaminantType': contaminantType,
                     'contaminantName': contaminantName }
-                    
+
+        # package would not install correctly, installed manually and set explicit path
+        config = pdfkit.configuration(wkhtmltopdf="/usr/local/bin/wkhtmltopdf")
+        options = {
+            'quiet': '',
+            'page-size': 'B7',
+            'margin-top': '0.35in',
+            'margin-right': '0.25in',
+            'margin-bottom': '0.25in',
+            'margin-left': '0.55in',
+            'disable-smart-shrinking': ''
+        }
+        pdfkit.from_file('surfertable/templates/chemical_summary_template.html', 'surfertable/static/hello.pdf', configuration = config, options = options)
+        response['pdfFile'] = 'hello.pdf'
+        # response = HttpResponse(content_type='application/pdf')
+        # response['Content-Disposition'] = 'attachment; filename="test.pdf"'
+
         #  only do lookup if values are from the available list
         if (contaminantType == contaminantTypeCas) or (contaminantType == contaminantTypeChemical):
             # convert CAS to chemical name
