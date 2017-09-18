fa76b8041847061b2c4a3882f93653f0ee845518
cn808 <cn808@macbook.com>
2017-09-17 15:25:48 -1000

    Remove unused code

--- EAL_Surfer/surferTable/admin.py
+++ EAL_Surfer/surferTable/admin.py
@@ -8,6 +8,4 @@ from django.apps import apps
 
 for model in apps.get_app_config('surferTable').models.values():
 
-    list_display = [field.name for field in model._meta.fields if field.name == "c1"]
-
     admin.site.register(model)
